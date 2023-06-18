49006c892eb30fca2278b2a19afa3aaaadb8ee10
Amanda1554 <amanda.malinga@gmail.com>
2023-06-18 16:21:29 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+  * main - Alphabet in both capital and small letters
+  *
+  * Return: Always 0 (Success)
+  **/
+int main(void)
+{
+	int n = 97;
+	int x = 65;
+
+	while (n < 123)
+	{
+		putchar(n);
+		n++;
+	}
+	while (x < 90)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
