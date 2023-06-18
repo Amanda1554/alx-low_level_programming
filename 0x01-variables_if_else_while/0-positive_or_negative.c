53f0ddb523ecaf7c026f46ecf612ede01a2ca229
Amanda1554 <amanda.malinga@gmail.com>
2023-06-18 14:37:20 -0700

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* main - code to check whether n is negagive, positive or zero */
-
-/* Return: Always 0 (Success) */
+/**
+  * main - code to check whether n is negagive, positive or zero
+  *
+  * Return: Always 0 (Success)
+  */
 int main(void)
 {
 	int n;
