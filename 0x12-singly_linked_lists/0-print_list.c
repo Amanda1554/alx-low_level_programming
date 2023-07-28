97075adbf66b4713abdc10a70f8c7a85649fdfb1
Amanda1554 <amanda.malinga@gmail.com>
2023-07-27 19:21:45 -0700

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,12 +13,12 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if (h == NULL)
+		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%lu] %s\n", i, h->str);
-		i++;
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
+		i++;
 	}
 	return (i);
 }
