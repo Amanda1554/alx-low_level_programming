7a3d01e931562070d4f1f77c618f149d499dad46
Amanda1554 <amanda.malinga@gmail.com>
2023-08-08 16:13:41 -0700

    Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+  * read_textfile - reads a text file and prints it to the POSIT stdout
+  * @letters: number of letters it should read and print
+  * @filename: file to be used
+  * Return: number of letters it could read and print, 0 if unsuccessful
+  */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t folder;
+	ssize_t read_byte;
+	char *buffer;
+
+	ssize_t written;
+
+	folder = open(filename, O_RDONLY);
+	if (folder == -1)
+		return (0);
+
+	buffer = malloc(letters * sizeof(char));
+	if (buffer == NULL)
+		return (0);
+
+	read_byte = read(folder, buffer, letters);
+	
+	written = write(STDOUT_FILENO, buffer, read_byte);
+	free(buffer);
+	close(folder);
+	return (written);
+}
